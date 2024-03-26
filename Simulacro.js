b7b4c3888a576453f28e01f8c9826926c3b8b7c3
Juan Ponce <juanvrp503@gmail.com>
2024-03-26 20:51:31 -0300

    Simulacro actualizado

---
+++ Simulacro.js
@@ -0,0 +1,18 @@
+function nota(numero){
+
+    if (numero <=4){
+
+        console.log("Libre");
+
+    } else if (numero >4 && numero < 7){
+
+        console.log("Regular"); 
+
+    } else { (numero > 6 )
+
+        console.log("Promocionado");
+    }
+
+}
+
+console.log(nota(7))
